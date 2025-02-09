1323deea82e5065c4e4a6530436697690074a0c1
Arthur Henrique <arthurcostalima88@gmail.com>
2025-02-08 21:35:30 -0300

    first commit

---
+++ src/Header/header.tsx
@@ -0,0 +1,28 @@
+function Header() {
+  return (
+    <header>
+      <div>
+        <img src="" />
+        <button id="glass">Solicitar Orçamento</button>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            Home <img src="../Pics/home.png" />{' '}
+          </li>
+          <li>
+            Empresa <img src="../Pics/building.png" />{' '}
+          </li>
+          <li>
+            Contato <img src="../Pics/contact.png" />{' '}
+          </li>
+          <li>
+            Referências <img src="../Pics/references.png" />{' '}
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
